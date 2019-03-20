c0ab7b657434822e1d262fa261ca035d3a851a86
Jide Okusanya <samuraialexis@gmail.com>
2019-03-20 12:16:20 +0100

    Added ciao from Babajide

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}> 
       <Image style={{width:50, height:50}} source={require('./pair1.png')}/>
-        <Text>Benvenuti nel mondo della Fight ju jitsu!</Text>
+        <Text>Benvenuti nel mondo della Fight ju jitsu! Ciao di Babajide</Text>
       </View>
     );
   }
