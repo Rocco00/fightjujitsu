ee8fe41c1c883a818d05f714f42d54f674c473c9
Rocco <rocco.biancardi@libero.it>
2019-03-20 12:12:44 +0100

    inizio

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}> 
+      <Image style={{width:50, height:50}} source={require('./pair1.png')}/>
+        <Text>Benvenuti nel mondo della Fight ju jitsu!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
