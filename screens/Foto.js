bec2400c0f26ab49b9f431b91c252b873f2d2a06
Rocco <rocco.biancardi@libero.it>
2019-03-26 12:28:17 +0100

    aggiunta di foto

--- screens/Foto.js
+++ screens/Foto.js
@@ -3,14 +3,24 @@ import {View,Text,TouchableOpacity,Image,StyleSheet} from "react-native";
 import { Ionicons } from '@expo/vector-icons';
 class Foto extends React.Component{
     render(){
+        const onegroup = ["photo1.jpeg","photo2.jpeg","photo3.jpeg","photo4.jpeg","photo5.jpg"];
+        const twogroup = ["settori.jpg"];
         return(
             <View style ={styles.grandebox}>
-            <TouchableOpacity onPress={()=>{
-           this.props.navigation.openDrawer()
-            }}>
-                <Ionicons style={{marginLeft:10,marginTop:40}} name="md-menu" size={32} color="green" />
-            </TouchableOpacity>
+                <TouchableOpacity onPress={()=>{
+                this.props.navigation.openDrawer()
+                }}>
+                    <Ionicons style={{marginLeft:10,marginTop:40}} name="md-menu" size={32} color="green" />
+                </TouchableOpacity>
                 <View style = {styles.box}>
+                <Image source = {require("../add_foto/"+"photo1.jpeg")} style = {styles.immagine}/>
+                {onegroup.map(function(foto){
+                    console.log("text")
+                    console.log("../add_foto/"+foto)
+                    const prova = "../add_foto/"+foto
+                    return <Image source = {{uri:prova}} style = {styles.immagine}/> 
+                })
+                }
                     <Text>
                         Foto
                     </Text>
@@ -29,6 +39,11 @@ const styles = StyleSheet.create({
     },
     grandebox:{
         flex:1,
+    },
+    immagine:{
+        height:140,
+        width:100,
     }
+
 })
 export default Foto;
