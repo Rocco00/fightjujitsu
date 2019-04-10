469dbb4ef25bae46a4f924ac30b31a9b142348f9
Rocco <rocco.biancardi@libero.it>
2019-04-10 12:19:59 +0200

    aggiunta di icona

--- screens/Home.js
+++ screens/Home.js
@@ -5,11 +5,20 @@ class Home extends React.Component{
     render(){
         return (
             <View style = {styles.grandebox}>
-                <TouchableOpacity onPress={()=>{
-                this.props.navigation.openDrawer()
-                }}>
-                    <Ionicons style={{marginLeft:10,marginTop:40}} name="md-menu" size={32} color="green" />
-                </TouchableOpacity>
+                <View style = {styles.riga}>
+                    <TouchableOpacity onPress={()=>{
+                        this.props.navigation.navigate("Foto") }}>
+                    <Image source ={require("../assets/camera.png")} style = {styles.icona} resizeMode = "contain" />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={()=>{
+                        this.props.navigation.navigate("Video") }}>
+                    <Image source ={require("../assets/youtube.png")} style = {styles.icona} resizeMode = "contain" />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={()=>{
+                        this.props.navigation.navigate("News") }}>
+                    <Image source ={require("../assets/newspaper.png")} style = {styles.icona} resizeMode = "contain" />
+                    </TouchableOpacity>
+                </View>
                 <View style = {styles.box}>
                     <Image source = {require("../pair1.jpeg")} style = {styles.principale} resizeMode = "contain" />
                     <Text style = {styles.scrittura}>
@@ -37,6 +46,20 @@ const styles = StyleSheet.create({
     scrittura:{
         fontSize:25,
         textAlign:"center",
+    },
+    riga:{
+        flexDirection:"row",
+        flexWrap:"wrap",
+        justifyContent:"center",
+        alignContent:"center",
+        alignItems:"center",
+        flex:1,
+    },
+    icona:{
+        width:80, 
+        height:80,
+        marginTop:150,
+        marginRight:5,
     }
 })
 export default Home;
